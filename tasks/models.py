486e412ed0f24c62f9b996776afcff401c9cd91c
toaha-unc <toahasiddique.ts@gmail.com>
2025-07-19 21:30:17 -0500

    add debug toolbar

--- tasks/models.py
+++ tasks/models.py
@@ -1,36 +1,38 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    # tasks
 
     def __str__(self):
         return self.name
 
 
 class Task(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('COMPLETED', 'Completed')
+    ]
     project = models.ForeignKey(
         "Project",
         on_delete=models.CASCADE,
         default=1
     )
     assigned_to = models.ManyToManyField(Employee, related_name='tasks')
-    # notun_string = models.CharField(max_length=100, default="")
     title = models.CharField(max_length=250)
     description = models.TextField()
     due_date = models.DateField()
+    status = models.CharField(
+        max_length=15, choices=STATUS_CHOICES, default="PENDING")
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # details
 
-# One to One
-# Many to One
-# Many to Many
+    def __str__(self):
+        return self.title
 
 
 class TaskDetail(models.Model):
@@ -45,20 +47,21 @@ class TaskDetail(models.Model):
     task = models.OneToOneField(
         Task,
         on_delete=models.CASCADE,
-        related_name='details'
+        related_name='details',
     )
     assigned_to = models.CharField(max_length=100)
     priority = models.CharField(
         max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
+    notes = models.TextField(blank=True, null=True)
 
-# Task.objects.get(id=2)
-# select * from task where id = 2
-# ORM
+    def __str__(self):
+        return f"Details form Task {self.task.title}"
 
 
 class Project(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
     start_date = models.DateField()
 
-# task = onekgula employee ekta task
-# employee = onekgula task er jonno assign ase
+    def __str__(self):
+        return self.name
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from tasks.views import manager_dashboard, user_dashboard, test, create_task
+from tasks.views import manager_dashboard, user_dashboard, test, create_task, view_task
 
 urlpatterns = [
     path('manager-dashboard/', manager_dashboard),
     path('user-dashboard/', user_dashboard),
     path('test/', test),
-    path('create-task/', create_task)
+    path('create-task/', create_task),
+    path('view-task/', view_task)
 ]
